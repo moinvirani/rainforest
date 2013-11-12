910bf4a363c3158b409f0a04c2e40c7f9c122d84
Moin Virani <moin.virani@gmail.com>
2013-11-11 21:19:33 -0500

    Updated the root in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rainforest::Application.routes.draw do
 
- 
+  root 'products#index'
   resources :products 
   resources :users, :only => [:new, :create]
   resources :sessions, :only => [:new, :create, :destroy]
@@ -8,7 +8,7 @@ Rainforest::Application.routes.draw do
   resources :products do
     resources :reviews, :except => [:index]
   end
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
